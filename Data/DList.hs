b13bf77c2a1ec55e661d6d221f000746c6ae719a
neubauer <neubauer@informatik.uni-freiburg.de>
2006-12-14 20:24:24 +0200

    add Functor and Monad instances
    darcs-hash:20061214182424-015fa-6ed88d1a3b8d17716a5a89f970597014ca90729f

--- Data/DList.hs
+++ Data/DList.hs
@@ -31,6 +31,8 @@ module Data.DList (
   ) where
 
 import Prelude hiding (concat)
+import Control.Monad
+import qualified Data.List as List (concat, map)
 import Data.Monoid
 
 -- | A difference list is a function that given a list, returns the
@@ -72,7 +74,24 @@ append xs ys = DL (unDL xs . unDL ys)
 concat       :: [DList a] -> DList a
 concat       = foldr append empty
 
-
 instance Monoid (DList a) where
     mempty  = empty
     mappend = append
+
+instance Functor DList where
+  fmap f = fromList . List.map f . toList
+
+instance Monad DList where
+  m >>= k  
+    -- = concat (toList (fmap k m))
+    -- = (concat . toList . fromList . List.map k . toList) m
+    -- = concat . List.map k . toList $ m
+    -- = foldr append empty . List.map k . toList $ m
+    = foldr (append . k) empty . toList $ m
+    
+  return x = singleton x
+  fail s   = empty
+
+instance MonadPlus DList where
+  mzero    = empty
+  mplus    = append
