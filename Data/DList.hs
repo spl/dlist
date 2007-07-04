7b6979abf52b7806460fc8cccdb7adb5c04c7fe7
Don Stewart <dons@cse.unsw.edu.au>
2007-07-04 07:38:11 +0200

    update DList.hs
    darcs-hash:20070704053811-9c5c1-334a6f544b792e693993fbfc0407828bb28f15e1

--- Data/DList.hs
+++ Data/DList.hs
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.DList
--- Copyright   :  (c) Don Stewart 2006
+-- Copyright   :  (c) Don Stewart 2006-2007
 -- License     :  BSD-style (see the file LICENSE)
 --
 -- Maintainer  :  dons@cse.unsw.edu.au
@@ -34,8 +34,8 @@ module Data.DList (
   ,foldr         -- :: (a -> b -> b) -> b -> DList a -> b
   ,map           -- :: (a -> b) -> DList a -> DList b
 
-  -- * Miscellaneous utilities
-  ,maybeToMonadPlus -- :: MonadPlus m => Maybe a -> m a
+  -- * MonadPlus
+  , maybeReturn
 
   ) where
 
@@ -54,38 +54,46 @@ newtype DList a = DL { unDL :: [a] -> [a] }
 -- | Converting a normal list to a dlist
 fromList    :: [a] -> DList a
 fromList    = DL . (++)
+{-# INLINE fromList #-}
 
 -- | Converting a dlist back to a normal list
 toList      :: DList a -> [a]
 toList      = ($[]) . unDL
+{-# INLINE toList #-}
 
 -- | Create a difference list containing no elements
 empty       :: DList a
 empty       = DL id
+{-# INLINE empty #-}
 
 -- | Create difference list with given single element
 singleton   :: a -> DList a
 singleton   = DL . (:)
+{-# INLINE singleton #-}
 
 -- | /O(1)/, Prepend a single element to a difference list
 infixr `cons`
 cons        :: a -> DList a -> DList a
 cons x xs   = DL ((x:) . unDL xs)
+{-# INLINE cons #-}
 
 -- | /O(1)/, Append a single element at a difference list
 infixl `snoc`
 snoc        :: DList a -> a -> DList a
 snoc xs x   = DL (unDL xs . (x:))
+{-# INLINE snoc #-}
 
--- | Appending difference lists
+-- | /O(1)/, Appending difference lists
 append       :: DList a -> DList a -> DList a
 append xs ys = DL (unDL xs . unDL ys)
+{-# INLINE append #-}
 
--- | Concatenate difference lists
+-- | /O(spine)/, Concatenate difference lists
 concat       :: [DList a] -> DList a
 concat       = List.foldr append empty
+{-# INLINE concat #-}
 
--- | /O(length dl)/, List elimination, head, tail
+-- | /O(length dl)/, List elimination, head, tail. 
 list :: b -> (a -> DList a -> b) -> DList a -> b
 list null cons dl =
   case toList dl of
@@ -98,7 +106,7 @@ head = list (error "Data.DList.head: empty list") (curry fst)
 
 -- | Return the tail of the list
 tail :: DList a -> DList a
-tail = list (error "Data.DList.tail: empty list") (curry snd) 
+tail = list (error "Data.DList.tail: empty list") (curry snd)
 
 -- | Unfoldr for difference lists
 unfoldr :: (b -> Maybe (a, b)) -> b -> DList a
@@ -110,11 +118,12 @@ unfoldr pf b =
 -- | Foldr over difference lists
 foldr        :: (a -> b -> b) -> b -> DList a -> b
 foldr f b    = List.foldr f b . toList
+{-# INLINE foldr #-}
 
--- | Map over difference lists
+-- | Map over difference lists.
 map          :: (a -> b) -> DList a -> DList b
 map f        = foldr (cons . f) empty
-
+{-# INLINE map #-}
 
 instance Monoid (DList a) where
     mempty  = empty
@@ -122,6 +131,7 @@ instance Monoid (DList a) where
 
 instance Functor DList where
     fmap = map
+    {-# INLINE fmap #-}
 
 instance Monad DList where
   m >>= k
@@ -131,15 +141,18 @@ instance Monad DList where
     -- = List.foldr append empty . List.map k . toList $ m
     -- = List.foldr (append . k) empty . toList $ m
     = foldr (append . k) empty m
+  {-# INLINE (>>=) #-}
 
   return x = singleton x
+  {-# INLINE return #-}
+
   fail s   = empty
+  {-# INLINE fail #-}
 
 instance MonadPlus DList where
   mzero    = empty
   mplus    = append
 
--- Use this to convert Maybe a into DList a, or indeed into any other
--- MonadPlus instance.
-maybeToMonadPlus :: MonadPlus m => Maybe a -> m a
-maybeToMonadPlus = maybe mzero return
+-- Use this to convert Maybe a into DList a, or indeed into any other MonadPlus instance.
+maybeReturn :: MonadPlus m => Maybe a -> m a
+maybeReturn = maybe mzero return
