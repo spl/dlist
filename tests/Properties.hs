794a54fecc13a3b1c5df866fd39a9ce20c23352d
Don Stewart <dons@cse.unsw.edu.au>
2007-07-04 07:35:31 +0200

    Update testsuite
    darcs-hash:20070704053531-9c5c1-89fce2b0a48eb203e58cb77508365a458243fa2b

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -1,7 +1,9 @@
 
-import qualified Prelude as P
-import Prelude          hiding (concat,map,head,tail)
-import Data.List        hiding (concat,map,head,tail)
+import qualified Prelude   as P
+import qualified Data.List as P (unfoldr)
+import Prelude          hiding (concat,map,head,tail,foldr,map)
+import Data.List        hiding (concat,map,head,tail,unfoldr,foldr,map)
+import Text.Show.Functions
 
 import Parallel
 import Data.DList
@@ -29,14 +31,48 @@ prop_head xs = not (null xs) ==> (P.head xs) == head (fromList xs)
 prop_tail xs = not (null xs) ==> (P.tail xs) == (toList . tail . fromList) xs
     where _ = xs :: T
 
-main = pRun 2 500 [ ("model",     pDet prop_model)
-                  , ("empty",     pDet prop_empty)
-                  , ("singleton", pDet prop_singleton)
-                  , ("cons",      pDet prop_cons)
-                  , ("snoc",      pDet prop_snoc)
-                  , ("append",    pDet prop_append)
-                  , ("concat",    pDet prop_concat)
-                  , ("head",      pDet prop_head)
-                  , ("tail",      pDet prop_tail)
-                  ]
+prop_unfoldr f x n = n >= 0 ==> take n (P.unfoldr f x)
+                             == take n (toList $ unfoldr f x)
+    where _ = x :: Int
+          _ = f :: Int -> Maybe (Int,Int)
 
+prop_foldr f x xs = (P.foldr f x xs) == (foldr f x (fromList xs))
+    where _ = x :: Int
+          _ = f :: Int -> Int -> Int
+
+prop_map f xs = (P.map f xs) == (toList $ map f (fromList xs))
+    where _ = f :: Int -> Int
+
+prop_map_fusion f g xs = (P.map f . P.map g $ xs)
+                      == (toList $ map f . map g $ fromList xs)
+    where _ = f :: Int -> Int
+
+--
+-- run 8 threads simultaneously
+--
+main = pRun 8 300
+    [ ("model",     pDet prop_model)
+    , ("empty",     pDet prop_empty)
+    , ("singleton", pDet prop_singleton)
+    , ("cons",      pDet prop_cons)
+    , ("snoc",      pDet prop_snoc)
+    , ("append",    pDet prop_append)
+    , ("concat",    pDet prop_concat)
+    , ("head",      pDet prop_head)
+    , ("tail",      pDet prop_tail)
+    , ("unfoldr",   pDet prop_unfoldr)
+    , ("foldr",     pDet prop_foldr)
+    , ("map",       pDet prop_map)
+    , ("map fusion",pDet prop_map)
+    ]
+
+
+------------------------------------------------------------------------
+--
+-- missing QC instances
+--
+
+instance Arbitrary a => Arbitrary (Maybe a) where
+  arbitrary           = do a <- arbitrary ; elements [Nothing, Just a]
+  coarbitrary Nothing = variant 0
+  coarbitrary _       = variant 1 -- ok?
