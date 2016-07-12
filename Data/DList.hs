e197f9465ba4b39f32825a96066782ead164e897
Sean Leather <sean.leather@gmail.com>
2016-07-12 12:51:28 +0200

    Update copyright year to 2016

--- Data/DList.hs
+++ Data/DList.hs
@@ -6,7 +6,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.DList
--- Copyright   :  (c) 2006-2009 Don Stewart, 2013-2015 Sean Leather
+-- Copyright   :  (c) 2006-2009 Don Stewart, 2013-2016 Sean Leather
 -- License     :  See LICENSE file
 --
 -- Maintainer  :  sean.leather@gmail.com
