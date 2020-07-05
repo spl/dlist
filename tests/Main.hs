4fa64e194d5bb0cb6e95a73133e50d166dfdacd1
Sean Leather <sean.leather@gmail.com>
2020-07-05 11:24:43 +0200

    wip

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,13 +1,24 @@
+-- Options passed to GHC
 {-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
 
+-----------------------------------------------------------------------------
+
 {-# LANGUAGE CPP #-}
-#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 708
-{-# LANGUAGE OverloadedLists #-} -- For the IsList test
-#if __GLASGOW_HASKELL__ == 708
-{-# LANGUAGE PatternSynonyms #-} -- For pattern synonym use only in GHC 7.8
-#endif
+
+#if !defined(__GLASGOW_HASKELL__)
+#error "Your compiler is not GHC. Let us know if dlist can be made to work on it."
 #endif
 
+-- CPP: GHC >= 7.8 for overloaded lists
+#if __GLASGOW_HASKELL__ >= 708
+-- For the IsList test
+{-# LANGUAGE OverloadedLists #-}
+#endif
+
+-- CPP: GHC == 7.8 for using pattern synonyms
+#if __GLASGOW_HASKELL__ == 708
+{-# LANGUAGE PatternSynonyms #-}
+#endif
 
 --------------------------------------------------------------------------------
 
@@ -15,19 +26,13 @@ module Main (main) where
 
 --------------------------------------------------------------------------------
 
-import Prelude hiding (concat, foldr, head, map, replicate, tail)
-
+import Data.DList
 import qualified Data.List as List
+import OverloadedStrings (testOverloadedStrings)
 import Test.QuickCheck
 import Text.Show.Functions ()
-
-import Data.DList
-
-import OverloadedStrings (testOverloadedStrings)
-
-#if !MIN_VERSION_base(4,8,0)
-import Data.Traversable (Traversable(traverse))
-#endif
+import Prelude hiding (concat, foldr, head, map, replicate, tail)
+import qualified Data.Traversable as Traversable
 
 #if MIN_VERSION_base(4,9,0)
 -- base-4.9 introduced Semigroup and NonEmpty.
@@ -54,10 +59,10 @@ prop_empty :: Bool
 prop_empty = ([] :: [Int]) == (toList empty :: [Int])
 
 prop_singleton :: Int -> Bool
-prop_singleton = eqWith (:[]) (toList . singleton)
+prop_singleton = eqWith (: []) (toList . singleton)
 
 prop_cons :: Int -> [Int] -> Bool
-prop_cons c = eqWith (c:) (toList . cons c . fromList)
+prop_cons c = eqWith (c :) (toList . cons c . fromList)
 
 prop_snoc :: [Int] -> Int -> Bool
 prop_snoc xs c = xs ++ [c] == toList (snoc (fromList xs) c)
@@ -110,27 +115,34 @@ prop_fail :: String -> Bool
 prop_fail str = fail str == (empty :: DList ())
 
 prop_Traversable_traverse :: [Int] -> Bool
-prop_Traversable_traverse xs =
-  (traverse (\x -> [x]) xs :: [[Int]]) == fmap toList (traverse (\x -> [x]) (fromList xs))
+prop_Traversable_traverse xs = (==)
+  (Traversable.traverse pure xs :: [[Int]])
+  (fmap toList (Traversable.traverse pure (fromList xs)))
 
-#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 708
+-- CPP: GHC >= 7.8 for overloaded lists
+#if __GLASGOW_HASKELL__ >= 708
+
 -- | Test that the IsList instance methods compile and work with simple lists
 prop_IsList :: Bool
-prop_IsList = test_fromList [1,2,3] && test_toList (fromList [1,2,3])
+prop_IsList = test_fromList [1,2,3] && test_toList (fromList [1, 2, 3])
   where
     test_fromList, test_toList :: DList Int -> Bool
-    test_fromList x = x == fromList [1,2,3]
-    test_toList [1,2,3] = True
-    test_toList _       = False
+    test_fromList x = x == fromList [1, 2, 3]
+    test_toList [1, 2, 3] = True
+    test_toList _ = False
 
+-- | Test the pattern synonyms
 prop_patterns :: [Int] -> Bool
 prop_patterns xs = case fromList xs of
-  Nil       -> xs == []
-  Cons y ys -> xs == (y:ys)
-  _         -> False
+  Nil -> xs == []
+  Cons y ys -> xs == (y : ys)
+  _ -> False
+
 #endif
 
+-- CPP: base >= 4.9 for Semigroup
 #if MIN_VERSION_base(4,9,0)
+
 prop_Semigroup_append :: [Int] -> [Int] -> Bool
 prop_Semigroup_append xs ys = xs <> ys == toList (fromList xs <> fromList ys)
 
@@ -155,39 +167,44 @@ prop_Semigroup_sconcat xs = sconcat xs == toList (sconcat (fmap fromList xs))
 prop_Semigroup_stimes :: Int -> [Int] -> Bool
 prop_Semigroup_stimes n xs =
   n < 0 || stimes n xs == toList (stimes n (fromList xs))
+
 #endif
 
 --------------------------------------------------------------------------------
 
 props :: [(String, Property)]
 props =
-  [ ("model",                property prop_model)
-  , ("empty",                property prop_empty)
-  , ("singleton",            property prop_singleton)
-  , ("cons",                 property prop_cons)
-  , ("snoc",                 property prop_snoc)
-  , ("append",               property prop_append)
-  , ("concat",               property prop_concat)
-  , ("replicate",            property prop_replicate)
-  , ("head",                 property prop_head)
-  , ("tail",                 property prop_tail)
-  , ("fail",                 property prop_fail)
-  , ("unfoldr",              property prop_unfoldr)
-  , ("foldr",                property prop_foldr)
-  , ("map",                  property prop_map)
-  , ("map fusion",           property (prop_map_fusion (+1) (+1)))
-  , ("intercalate",          property prop_intercalate)
-  , ("read . show",          property prop_show_read)
-  , ("show . read",          property prop_read_show)
-  , ("Traversable traverse", property prop_Traversable_traverse)
-#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 708
-  , ("IsList",               property prop_IsList)
-  , ("patterns",             property prop_patterns)
+  [ ("model", property prop_model),
+    ("empty", property prop_empty),
+    ("singleton", property prop_singleton),
+    ("cons", property prop_cons),
+    ("snoc", property prop_snoc),
+    ("append", property prop_append),
+    ("concat", property prop_concat),
+    ("replicate", property prop_replicate),
+    ("head", property prop_head),
+    ("tail", property prop_tail),
+    ("fail", property prop_fail),
+    ("unfoldr", property prop_unfoldr),
+    ("foldr", property prop_foldr),
+    ("map", property prop_map),
+    ("map fusion", property (prop_map_fusion (+ 1) (+ 1))),
+    ("intercalate", property prop_intercalate),
+    ("read . show", property prop_show_read),
+    ("show . read", property prop_read_show),
+    ("Traversable traverse", property prop_Traversable_traverse)
+-- CPP: GHC >= 7.8 for IsList, pattern synonyms
+#if __GLASGOW_HASKELL__ >= 708
+    ,
+    ("IsList",               property prop_IsList),
+    ("patterns",             property prop_patterns)
 #endif
+-- CPP: base >= 4.9 for Semigroup
 #if MIN_VERSION_base(4,9,0)
-  , ("Semigroup <>",         property prop_Semigroup_append)
-  , ("Semigroup sconcat",    property prop_Semigroup_sconcat)
-  , ("Semigroup stimes",     property prop_Semigroup_stimes)
+    ,
+    ("Semigroup <>",         property prop_Semigroup_append),
+    ("Semigroup sconcat",    property prop_Semigroup_sconcat),
+    ("Semigroup stimes",     property prop_Semigroup_stimes)
 #endif
   ]
 
