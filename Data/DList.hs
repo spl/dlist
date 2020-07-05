10865ba1d8341e1cd5445bc5c3324237f0f1f3a2
Sean Leather <sean.leather@gmail.com>
2020-07-05 10:52:36 +0200

    Restore some of Data.DList to minimize diff

--- Data/DList.hs
+++ Data/DList.hs
@@ -1,86 +1,76 @@
 {-# LANGUAGE CPP #-}
 
-#if !defined(__GLASGOW_HASKELL__)
-#error "Your compiler is not GHC. Let us know if dlist can be made to work on it."
-#endif
-
--- CPP: GHC >= 7.8 for Safe Haskell
-#if __GLASGOW_HASKELL__ >= 708
--- The 'Data.DList' module imports the unsafe module 'Data.DList.Unsafe' but
--- exports only its safe aspects. Specifically, it does not export the 'DList'
--- constructor 'UnsafeDList' or record label 'unsafeApplyDList'. Therefore, we
--- mark 'Data.DList' as trustworthy.
-{-# LANGUAGE Trustworthy #-}
-#endif
-
 -- CPP: GHC >= 7.8 && <= 8 for 'pattern' required in the export list
-#if __GLASGOW_HASKELL__ >= 708 && __GLASGOW_HASKELL__ < 800
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 708 && __GLASGOW_HASKELL__ < 800
 {-# LANGUAGE PatternSynonyms #-}
 #endif
 
------------------------------------------------------------------------------
+-- CPP: GHC >= 7.8 for Safe Haskell
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 708
+{-
 
-{-|
-
-Module: Data.DList
-Copyright: © 2006-2009 Don Stewart, 2013-2020 Sean Leather
-License: BSD-3-Clause
-
-Maintainer: sean.leather@gmail.com
-Stability: stable
-
-A __difference list__ is an abstraction representing a list that
-supports&#x00A0;\(\mathcal{O}\)(@1@) 'append' and 'snoc' operations. This module
-provides the type for a difference list, 'DList', and a collection of supporting
-functions for (a) converting to and from lists and (b) operating on 'DList's
-efficiently.
+The 'Data.DList' module imports the unsafe module 'Data.DList.Unsafe' but
+exports only its safe aspects. Specifically, it does not export the 'DList'
+constructor 'UnsafeDList' or record label 'unsafeApplyDList'. Therefore, we mark
+'Data.DList' as trustworthy.
 
 -}
-
-module Data.DList
-  ( -- * Difference List Type
-    DList
-
--- CPP: GHC >= 8 for pattern synonyms allowed in the constructor
-#if __GLASGOW_HASKELL__ >= 800
-    (Nil, Cons),
-#else
-    ,
--- CPP: GHC >= 7.8 && <= 8 for 'pattern' required in the export list
-#if __GLASGOW_HASKELL__ >= 708
-    -- ** Bundled Patterns
-    pattern Nil,
-    pattern Cons,
+{-# LANGUAGE Trustworthy #-}
 #endif
-#endif
-
-    -- * Conversion
-    fromList,
-    toList,
-    apply,
-
-    -- * Basic Functions
-    empty,
-    singleton,
-    cons,
-    snoc,
-    append,
-    concat,
-    replicate,
-    list,
-    head,
-    tail,
-    unfoldr,
-    foldr,
-    map,
-    intercalate,
-  )
-where
 
 -----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.DList
+-- Copyright   :  (c) 2006-2009 Don Stewart, 2013-2020 Sean Leather
+-- License     :  See license.md file
+--
+-- Maintainer  :  sean.leather@gmail.com
+-- Stability   :  stable
+-- Portability :  portable
+--
+-- Difference lists: a data structure for /O(1)/ append on lists.
+--
+-----------------------------------------------------------------------------
 
-import Data.DList.Unsafe
+module Data.DList
+
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 800
+  ( DList(Nil, Cons)
+#else
+  ( DList
+#endif
+
+  -- * Construction
+  , fromList
+  , toList
+  , apply
+
+  -- * Basic functions
+  , empty
+  , singleton
+  , cons
+  , snoc
+  , append
+  , concat
+  , replicate
+  , list
+  , head
+  , tail
+  , unfoldr
+  , foldr
+  , map
+  , intercalate
+
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 708 && __GLASGOW_HASKELL__ < 800
+  -- * Pattern Synonyms
+  , pattern Nil
+  , pattern Cons
+#endif
+
+  ) where
 
 -- The 'Data.DList' module exists only to export names from 'Data.DList.Unsafe'.
 -- Some names conflict with 'Prelude', so we hide all imports from 'Prelude'.
 import Prelude ()
+
+import Data.DList.Unsafe
