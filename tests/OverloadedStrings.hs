4d8d351f540befb4423afb97139ea8272daa9045
Sean Leather <sean.leather@gmail.com>
2020-07-09 13:25:44 +0200

    Fix OverloadedStrings

--- tests/OverloadedStrings.hs
+++ tests/OverloadedStrings.hs
@@ -4,6 +4,12 @@
 
 --------------------------------------------------------------------------------
 
+{-# LANGUAGE CPP #-}
+
+#if !defined(__GLASGOW_HASKELL__)
+#error "Your compiler is not GHC. Let us know if dlist can be made to work on it."
+#endif
+
 {-# LANGUAGE OverloadedStrings #-}
 {- ORMOLU_ENABLE -}
 
@@ -15,11 +21,17 @@ module OverloadedStrings (test) where
 --------------------------------------------------------------------------------
 
 import qualified Data.DList as DList
+-- CPP: GHC >= 8 for DNonEmpty
+#if __GLASGOW_HASKELL__ >= 800
 import qualified Data.DList.DNonEmpty as DNonEmpty
+#endif
 
 --------------------------------------------------------------------------------
 
 test :: IO ()
 test = do
   print $ "OverloadedStrings for DList:     " `DList.append` "success"
+-- CPP: GHC >= 8 for DNonEmpty
+#if __GLASGOW_HASKELL__ >= 800
   print $ "OverloadedStrings for DNonEmpty: " `DNonEmpty.append` "success"
+#endif
