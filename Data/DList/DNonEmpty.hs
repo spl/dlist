c2549b7fdd8576c688e7f03aa7135a5fb2b431f5
Sean Leather <sean.leather@gmail.com>
2020-07-11 05:00:18 +0200

    Add DNonEmpty (#60)
    * Create `Data.DList.DNonEmpty`
    * Add tests for `DNonEmpty`

---
+++ Data/DList/DNonEmpty.hs
@@ -0,0 +1,74 @@
+{- ORMOLU_DISABLE -}
+{-# LANGUAGE CPP #-}
+
+-----------------------------------------------------------------------------
+
+-- CPP: Ignore unused imports when Haddock is run
+#if defined(__HADDOCK_VERSION__)
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+#endif
+
+-----------------------------------------------------------------------------
+
+{-|
+
+Module: Data.DList.DNonEmpty
+Copyright: © 2017-2020 Oleg Grenrus, 2020 Sean Leather
+License: BSD-3-Clause
+
+Maintainer: sean.leather@gmail.com
+Stability: stable
+
+A __non-empty difference list__ is a difference list paired with a 'head'
+element. Like the difference list, it supports&#x00A0;\(\mathcal{O}\)(@1@)
+'append' and 'snoc' operations.
+
+This module provides the type for a non-empty difference list, 'DNonEmpty', and
+a collection of supporting functions for (a) converting to and from 'NonEmpty'
+and 'DList' and (b) operating efficiently on 'DNonEmpty' values. The functions
+also retain the non-strict semantics of 'NonEmpty'.
+
+-}
+{- ORMOLU_ENABLE -}
+
+module Data.DList.DNonEmpty
+  ( -- * Non-Empty Difference List Type
+    DNonEmpty((:|)),
+
+    -- * Conversion
+    fromNonEmpty,
+    toNonEmpty,
+    toDList,
+
+    -- * Basic Functions
+    singleton,
+    cons,
+    snoc,
+    append,
+    head,
+    tail,
+    unfoldr,
+    map,
+  )
+where
+
+-----------------------------------------------------------------------------
+
+import Data.DList.DNonEmpty.Internal
+
+-- CPP: Import only for Haddock
+#if defined(__HADDOCK_VERSION__)
+import Data.List.NonEmpty (NonEmpty)
+import Data.DList.Unsafe (DList)
+#endif
+
+{- ORMOLU_DISABLE -}
+{-
+
+The 'Data.DList.DNonEmpty' module exists only to export names from
+'Data.DList.DNonEmpty.Internal'. Some names conflict with 'Prelude', so we hide
+all imports from 'Prelude'.
+
+-}
+{- ORMOLU_ENABLE -}
+import Prelude ()
--- tests/Main.hs
+++ tests/Main.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE CPP #-}
+
+#if !defined(__GLASGOW_HASKELL__)
+#error "Your compiler is not GHC. Let us know if dlist can be made to work on it."
+#endif
+
+--------------------------------------------------------------------------------
+
 -- | Test runner.
 module Main (main) where
 
 --------------------------------------------------------------------------------
 
 import qualified DListProperties
+-- CPP: GHC >= 8 for DNonEmpty
+#if __GLASGOW_HASKELL__ >= 800
+import qualified DNonEmptyProperties
+#endif
 import qualified OverloadedStrings
 
 --------------------------------------------------------------------------------
@@ -11,4 +23,8 @@ import qualified OverloadedStrings
 main :: IO ()
 main = do
   DListProperties.test
+-- CPP: GHC >= 8 for DNonEmpty
+#if __GLASGOW_HASKELL__ >= 800
+  DNonEmptyProperties.test
+#endif
   OverloadedStrings.test
